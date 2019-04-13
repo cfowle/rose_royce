2a10a89ff1d19db83d30663d1ff62cb647327106
cfowle <carriemfowle@gmail.com>
2019-04-13 17:54:57 -0400

    initial commit

---
+++ code/0_master.R
@@ -0,0 +1,11 @@
+###############################################################################
+##### PROJECT: ROSE-ROYCE
+##### PURPOSE: CLEANS RAW DATA
+###############################################################################
+
+library(lubridate)
+library(magrittr)
+library(stringr)
+library(tidyverse)
+
+setwd("~/code/rose-royce/code")
